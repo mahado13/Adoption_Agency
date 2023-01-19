c71b15f2aa0d49e6028d6d47ce3ea8cce97c817f
Mahad Osman <mahado14@gmail.com>
2023-01-19 13:36:17 -0500

    Step 3 & 4 complete

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 """
 By:Mahad Osman
@@ -24,6 +25,23 @@ def show_home():
     pets = Pet.query.all()
     return render_template('home.html', pets = pets)
 
-@app.route('/pets/add', methods=["GET", "POSTS"])
+@app.route('/pets/add', methods=["GET", "POST"])
 def add_new_pet():
-    return render_template('new_pet.html')
+    """Our route to add a new pet, and retrieve the form to do so."""
+    form = AddPetForm()
+    print(request.form)
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        pet_photo = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        pet = Pet(name=name, species=species, photo_url=pet_photo, age=age, notes=notes, available=available)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('new_pet.html', form = form)
--- forms.py
+++ forms.py
@@ -8,3 +8,11 @@ Date: Jan 19
 Assignment: Adoption Agency 
 """
 
+class AddPetForm(FlaskForm):
+    """Form for adding a new pet!"""
+    name =StringField("Pet name:")
+    species =StringField("Species:")
+    photo_url = StringField("Pet Photo:")
+    age =IntegerField("Pet age:") 
+    notes =StringField("Enter relevant notes:")
+    available = BooleanField("Pet available?")
